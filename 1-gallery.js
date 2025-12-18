4333c1620ba3888e8015b276dbd6e67d02b5f54d
shumskiy5524 <shumskiparents@gmail.com>
2025-12-18 22:06:53 +0000

    Deploying to gh-pages from @ shumskiy5524/goit-js-hw-09@eca1742f7b410d8fbaed817670e7d70222f5eb4f 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,12 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as p}from"./assets/vendor-CgTBfC_f.js";const t=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",description:"Aerial Beach View"},{preview:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",original:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",description:"Flower Blooms"},{preview:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",original:"https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",description:"Alpine Mountains"}],e=document.querySelector(".gallery"),n=t.map(({preview:o,original:i,description:a})=>`
-    <li class="gallery-item">
-      <a class="gallery-link" href="${i}">
-        <img 
-          class="gallery-image" 
-          src="${o}" 
-          alt="${a}" 
-        />
-      </a>
-    </li>
-  `).join("");e.innerHTML=n;new p(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as t}from"./assets/vendor-CgTBfC_f.js";document.querySelector(".gallery");new t(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
